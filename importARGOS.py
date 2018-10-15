4715f87b2ac828473057209e7f597a24feeacbf3
kdelyser <kendall.delyser@duke.edu>
2018-10-15 12:09:29 -0400

    Initial commit of ARGOS script

---
+++ importARGOS.py
@@ -0,0 +1,11 @@
+##
+## importARGOS.py
+##
+## Description: Read in ARGOS formatted tracking data and create a line feature class from the [filtered] tracking points
+##
+## Usage: importARGOS <ARGOS folder> <Output feature class>
+##
+## Created: Oct 2018 (for ENV859)
+## Author: Kendall DeLyser, kendall.delyser@duke.edu
+##
+
